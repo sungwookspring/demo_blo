32c3b4e4a598a94195141a97f218c5fdbeee2337
choisungwook <kgg1959@naver.com>
2020-08-06 17:17:12 +0900

    [생성] 회원가입: 회원가입 페이지, 회원가입 실패 페이지(이미 존재하는 회원)

--- src/main/java/sungwook/demo_blog/controller/MemberController.java
+++ src/main/java/sungwook/demo_blog/controller/MemberController.java
@@ -2,6 +2,9 @@ package sungwook.demo_blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sungwook.demo_blog.domain.Member;
 import sungwook.demo_blog.service.MemberService;
 
 @Controller
@@ -12,5 +15,33 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberserive) {
         this.memberserive = memberserive;
+    }
+
+    /*
+        회원가입 폼
+    */
+    @GetMapping("/member/new")
+    public String CreateForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/member/new")
+    public String createUser(MemberForm memberform){
+        Member member = new Member();
+        member.setName(memberform.getName());
+        
+        //회원가입
+        //이미 회원이 있는 경우 에러 페이지 이동
+        try{
+            memberserive.join(member);
+        }catch(IllegalStateException e){
+            boolean result = e.getMessage().equals("이미 존재하는 회원입니다");
+            if(result==true){
+                System.out.println("이미 존재하는 회원");
+                return "members/joinerror.html";
+            }
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/sungwook/demo_blog/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package sungwook.demo_blog.controller;
+
+public class MemberForm {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
